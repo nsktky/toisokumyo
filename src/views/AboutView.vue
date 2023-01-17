d35123db45a14f63114350e09cd194bd74a384fe
nsktky <nosaka.takuya11@gmail.com>
2023-01-17 21:10:40 +0900

    work素案

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,75 @@
 <template>
-  <div class="about">
-    <h1>about</h1>
+  <v-card
+    class="mx-auto"
+    color="#ffffff80"
+    align-content="center"
+  >
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            color="#385F7380"
+            dark
+          >
+            <v-card-title class="text-h5">
+              Unlimited music now
+            </v-card-title>
 
-  </div>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>
+                Listen Now
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card
+            color="#385F7380"
+            dark
+          >
+            <v-card-title class="text-h5">
+              Unlimited music now
+            </v-card-title>
+
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>
+                Listen Now
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card
+            color="#385F7380"
+            dark
+          >
+            <v-card-title class="text-h5">
+              Unlimited music now
+            </v-card-title>
+
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>
+                Listen Now
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <style scoped>
-.about {
+.mx-auto {
   width: 100%;
-  margin-top: 1rem;
-  padding: auto;
+  margin: 1rem auto;
+  padding: 0 auto;
   position: absolute;
 }
 </style>
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,16 +1,49 @@
+
 <template>
-  <div class="work">
-    <h1>Work</h1>
-    <iframe class="frame" height="315" style="max-width: 560px; width: 100%; overflow:hidden; display:block;" src="https://neort.io/embed/cei4bmcn70rvflp1fj3g?autoStart=false&quality=1&info=false" frameborder="0" sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-downloads" allow="geolocation; microphone; camera; midi; vr" allowfullscreen="true" allowtransparency="true"></iframe>
-    <iframe class="frame" height="315" style="max-width: 560px; width: 100%; overflow:hidden; display:block;" src="https://neort.io/embed/cego5hsn70rvflp1fa60?autoStart=false&quality=1&info=true" frameborder="0" sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-downloads" allow="geolocation; microphone; camera; midi; vr" allowfullscreen="true" allowtransparency="true"></iframe>
-    <h2>
-      <a href="https://neort.io/@IWD3ncMyhLhqMb7dInMwosr9Yw92">more</a>
-    </h2>
-  </div>
+  <v-app id="inspire" class="work">
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row justify="center" align-content="center">
+            <!-- v-forでimagesの各要素をitemに格納 -->
+          <v-col
+            v-for="item in images" :key="item.src"
+          >
+            <v-card  height="80vh">
+                <!-- itemsから表示するデータを取り出す -->
+                <a v-bind:href="item.link">
+                  <v-img :src="item" height="80vh"></v-img>
+                </a>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-.work {
+<script>
+  export default {
+      data: () => ({
+    //   画像のpath,タイトル,NEORTのリンク.画像はassetsに格納しているためsrcにrequire()で渡す.
+      images: [
+        {src: require('@/assets/images/art1.jpg'),
+         title: 'フレームワーク',
+         link: 'https://neort.io/art/cbd87g43p9f2v73urnfg?index=0&origin=user_profile'},
+        {src: require('@/assets/images/art2.jpg'),
+         title: '規定',
+         link: 'https://neort.io/art/cb8k5p43p9f2v73ur4lg?index=7&origin=user_profile'},
+        {src: require('@/assets/images/art3.jpg'),
+         title: 'REM',
+         link: 'https://neort.io/art/c6o9dl43p9f1mfrqjvpg?index=228&origin=user_profile'},
+        
+      ]
+    }),
+  }
+</script>
+
+<style>
+.work{
+  height: 90vh;
   width: 100%;
   margin-top: 1rem;
   padding: auto;
